77062fc3b7ad005e6646eb26f742efd6723c64a1
virginianjoki <njokivirginia0619@gmail.com>
2024-10-23 14:47:44 +0300

    book_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -9,23 +9,23 @@ CREATE TABLE Books (
     publication_date DATE
 );
 CREATE TABLE Authors (
-    author_id(Primary Key)
-    author_name VARCHAR(215)
+    author_id(Primary Key),
+    author_name VARCHAR(215),
 );
 CREATE TABLE Customers (
-    customer_id(Primary Key)
-    customer_name VARCHAR(215)
-    email VARCHAR(215)
-    address TEXT
+    customer_id(Primary Key),
+    customer_name VARCHAR(215,)
+    email VARCHAR(215),
+    address TEXT,
 ); 
 CREATE TABLE orders (
-  order_id(Primary Key)
-  customer_id(Foreign Key referencing Customers table)
-  order_date DATE
+  order_id(Primary Key),
+  customer_id(Foreign Key referencing Customers table),
+  order_date DATE,
 );
 CREATE TABLE Order_Details (
-    order_detail/id(Primary Key)
-    order_id(Foreign Key referencing Orders table)
-    book_id (Foreign Key referencing Books table)
-    quantity DOUBLE
+    order_detail/id(Primary Key),
+    order_id(Foreign Key referencing Orders table),
+    book_id (Foreign Key referencing Books table),
+    quantity DOUBLE,
 );
