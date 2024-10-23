c8f903c4e64fa4a94da8a1c38a75d56c6a62c0a0
virginianjoki <njokivirginia0619@gmail.com>
2024-10-23 14:42:06 +0300

    book_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -7,25 +7,25 @@ CREATE TABLE Books(
     author_id(Foreign Key referncing Authors table),
     price DOUBLE,
     publication_date DATE
-)
+);
 CREATE TABLE Authors(
     author_id(Primary Key)
     author_name VARCHAR(215)
-)
+);
 CREATE TABLE Customers(
     customer_id(Primary Key)
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address TEXT
-) 
+); 
 CREATE TABLE orders(
   order_id(Primary Key)
   customer_id(Foreign Key referencing Customers table)
   order_date DATE
-)
+);
 CREATE TABLE Order_Details(
     order_detail/id(Primary Key)
     order_id(Foreign Key referencing Orders table)
     book_id (Foreign Key referencing Books table)
     quantity DOUBLE
-)
+);
