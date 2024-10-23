227ecfa435ea9cd066886d7f3896f5a2afd3fc45
virginianjoki <njokivirginia0619@gmail.com>
2024-10-23 14:44:23 +0300

    book_store

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,29 +1,29 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE TABLE Books(
+CREATE TABLE Books (
     book_id(primary key),
     title VARCHAR(130),
     author_id(Foreign Key referncing Authors table),
     price DOUBLE,
     publication_date DATE
 );
-CREATE TABLE Authors(
+CREATE TABLE Authors (
     author_id(Primary Key)
     author_name VARCHAR(215)
 );
-CREATE TABLE Customers(
+CREATE TABLE Customers (
     customer_id(Primary Key)
     customer_name VARCHAR(215)
     email VARCHAR(215)
     address TEXT
 ); 
-CREATE TABLE orders(
+CREATE TABLE orders (
   order_id(Primary Key)
   customer_id(Foreign Key referencing Customers table)
   order_date DATE
 );
-CREATE TABLE Order_Details(
+CREATE TABLE Order_Details (
     order_detail/id(Primary Key)
     order_id(Foreign Key referencing Orders table)
     book_id (Foreign Key referencing Books table)
