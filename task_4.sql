ebb7ee4392124869f61f6cf4158f076db7bf85ff
virginianjoki <njokivirginia0619@gmail.com>
2024-10-24 16:08:54 +0300

    task4

--- task_4.sql
+++ task_4.sql
@@ -1,12 +1,12 @@
 SELECT
-    COLUMN_NAME AS 'Column_name'
-    COLUMN_TYPE AS 'Column_type'
-    IS NULLABLE AS 'Is nullable'
-    COLUMN_KEY AS 'Key'
-    COLUMN_DEFAULT AS 'Default'
+    COLUMN_NAME AS 'Column_name',
+    COLUMN_TYPE AS 'Column_type',
+    IS_NULLABLE AS 'Is_nullable',
+    COLUMN_KEY AS 'Key',
+    COLUMN_DEFAULT AS 'Default',
     EXTRA AS 'Extra'
 FROM
     INFORMATION_SCHEMA.COLUMNS
 WHERE
-    TABLE_SCHEMA ='alx_book_store'
-    AND TABLE_NAME ='Books';
+    TABLE_SCHEMA = 'alx_book_store'
+    AND TABLE_NAME = 'books';
